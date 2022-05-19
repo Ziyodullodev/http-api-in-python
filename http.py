c7dde6bf0ea145023c90d8cd047849a145439cca
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2022-05-18 22:06:42 -0700

    Create http.py
    first api code in python

---
+++ http.py
@@ -0,0 +1,51 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from json import dumps
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+
+  def _send_cors_headers(self):
+
+      self.send_header("Access-Control-Allow-Origin", "*")
+      self.send_header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+      self.send_header("Access-Control-Allow-Headers", "x-api-key,Content-Type")
+
+  def send_dict_response(self, d):
+      """ Sends a dictionary (JSON) back to the client """
+      self.wfile.write(bytes(dumps(d), "utf8"))
+
+  def do_OPTIONS(self):
+      self.send_response(200)
+      self._send_cors_headers()
+      self.end_headers()
+
+  def do_GET(self):
+      self.send_response(200)
+      self._send_cors_headers()
+      self.end_headers()
+
+      response = {}
+      response["status"] = "OK"
+      self.send_dict_response(response)
+
+  def do_POST(self):
+      self.send_response(200)
+      self._send_cors_headers()
+      self.send_header("Content-Type", "application/json")
+      self.end_headers()
+
+      dataLength = int(self.headers["Content-Length"])
+      data = self.rfile.read(dataLength)
+
+      print(data)
+
+      response = {}
+      response["status"] = "OK"
+      self.send_dict_response(response)
+
+
+print("Starting server")
+httpd = HTTPServer(("127.0.0.1", 1000), RequestHandler)
+
+print("Hosting server on port 8000")
+httpd.serve_forever()
